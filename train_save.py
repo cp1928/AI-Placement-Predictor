5c2a6dd20db85baea1e80192d418b35ac695e068
cp1928 <chitraparashuram28@gmail.com>
2026-02-03 18:35:41 +0530

    Added model evaluation metrics and updated Streamlit pipeline

--- train_save.py
+++ train_save.py
@@ -3,17 +3,22 @@ import pickle
 from src.data_preprocessing import preprocess_data
 from src.model_training import train_model
 
-# Load data
-df = pd.read_csv(r"C:\Users\cp055\Desktop\a-p-p\data\student_placement_predictor_dataset.csv")
+df = pd.read_csv(r".......\data\student_placement_predictor_dataset.csv")
 
-# Preprocess
+
 X_train, X_test, y_train, y_test, scaler = preprocess_data(df)
 
-# Train model
 model = train_model(X_train, y_train)
 
-# Save model and scaler
+from src.model_training import evaluate_model
+accuracy, report, confusion_mat = evaluate_model(model, X_test, y_test)
+
+print(f"Model Accuracy: {accuracy * 100:.2f}%")
+print("\nClassification Report:\n", report)
+print("\nConfusion Matrix:\n", confusion_mat)
+
+
 pickle.dump(model, open("app/model.pkl", "wb"))
 pickle.dump(scaler, open("app/scaler.pkl", "wb"))
 
-print("Model and scaler saved successfully!")
+print("\nModel, scaler, and evaluation metrics generated successfully!")
